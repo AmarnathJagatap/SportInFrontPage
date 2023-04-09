71c8850275c4ee37b581790ae16d06f342a9448c
amarnathjagatap <amarnathjjagatap@gmail.com>
2023-04-10 00:07:52 +0530

    Updates

--- src/Products/Pages/GameZone.jsx
+++ src/Products/Pages/GameZone.jsx
@@ -6,12 +6,13 @@ const GameZone = () => {
   return (
     <>
     <Navbar1/>
-    <div name='carracing' className='h-screen w-full pt-28 py-10 bg-white'>
+    <div name='carracing' className='w-full pt-28 py-10 bg-white'>
         <div className='flex flex-col items-center max-w-[1240px] mx-auto'>
-            <div className='text-center'>
-               <p className="text-lg text-left font-SignikaNegative">At SportIn we have built state of the art tool to help game zone owners build and conceptualize their game zones.</p>
-               <h1 className='md:text-4xl sm:text-3xl text-center font-Rubik text-black text-2xl font-bold py-2'>Coming Soon...</h1>
-            </div>           
+        <h1 className='md:text-4xl sm:text-3xl text-center font-Rubik text-black text-2xl font-bold py-2'>Game Zone</h1>
+
+              <img src='https://res.cloudinary.com/dbwqjycvh/image/upload/v1681065073/realistic-coming-soon-background_52683-59078_qko5gn.jpg' className='my-10 rounded-lg hover:animate-pulse w-[400px] h-[400px]'/>
+               <p className="text-lg text-center font-SignikaNegative">At SportIn we have built state of the art tool to help game zone owners build and conceptualize their game zones.</p>
+               
         </div>
     </div>
     <Footer/>
--- src/components/AboutUS.jsx
+++ src/components/AboutUS.jsx
@@ -4,57 +4,37 @@ import Fade from 'react-reveal/Fade';
 const About = () => {
   return (
     <div name="about"  className='w-full bg-white'>
-       <div className='hidden md:flex grid md:grid-cols-2'>
+       <div className='grid md:grid-cols-2'>
+       <div className="block md:hidden">
+              <Fade right>
+            <div class="h-screen flex flex-col relative  justify-center items-center">
+                <img src='https://res.cloudinary.com/dbwqjycvh/image/upload/v1680512780/boyenjoy_bm53m9.jpg' className='hover:animate-pulse w-full'/>
+            </div>
+            </Fade> 
+            </div>    
             <div>  
-            <div className=" h-screen flex flex-col relative  justify-center items-center shadow-xl">
+            <div class="h-screen flex flex-col relative  justify-center items-center shadow-xl">
             <Fade left>         
               
-                 <h1 className="text-[#525252] text-center sm:text-5xl text-5xl font-SignikaNegative relative z-20">What We Do?</h1>
-                <p className="mx-4 text-[#525252] text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">At Sportin Leisure, we specialize in creating exceptional leisure and entertainment destinations for our clients. We understand the importance of understanding our clients' needs, which is why we work closely with them to understand their vision, project brief, and ideal budget. We also take into consideration the target clientele, project feasibility, and site analysis to cater to the demographic.
+                 <h1 className="text-[#525252] text-center sm:text-5xl text-5xl font-SignikaNegative relative z-20">ABOUT US</h1>
+                <p className="text-[#525252] mx-4 text-justify mt-2 sm:text-lg text-lg font-SignikaNegative relative z-20">At Sportin Entertainment Solutions, we specialize in creating exceptional leisure and entertainment destinations for our clients. We understand the importance of understanding our clients' needs, which is why we work closely with them to understand their vision, project brief, and ideal budget. We also take into consideration the target clientele, project feasibility, and site analysis to cater to the demographic.
                 </p>
-                <p className="mx-4 text-[#525252] text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">
-We create and ideate by focusing on brand value, master planning, conceptualization, themed ambiances, schematic and detailed design. Our aim is to realize the creation by providing design supervision, project management, commissioning, pre-opening, and soft launch services.
-</p>
-<p className="text-[#525252] mx-4 text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">
-We provide post-launch support, including operations management, revenue analysis, marketing and promotion, and maximizing revenues. Our goal is to ensure that our clients' leisure facilities are not just operational but successful in the long term.</p>
+                <p className="text-[#525252] mx-4 text-justify mt-2 sm:text-lg text-lg font-SignikaNegative relative z-20">
+We create and ideate by focusing on brand value, master planning, conceptualization, themed ambiances, schematic and detailed design. Our aim is to realize the creation by providing design supervision, project management, commissioning, pre-opening, and soft launch services.</p>
+<p className="text-[#525252] text-justify mt-2 mx-4 sm:text-lg text-lg font-SignikaNegative relative z-20">We provide post-launch support, including operations management, revenue analysis, marketing and promotion, and maximizing revenues. Our goal is to ensure that our clients' leisure facilities are not just operational but successful in the long term.</p>
                 </Fade>
 
             </div> 
             </div>
-            <div>
+            <div className="hidden md:block">
               <Fade right>
             <div class="h-screen flex flex-col relative  justify-center items-center">
                 <img src='https://res.cloudinary.com/dbwqjycvh/image/upload/v1680512780/boyenjoy_bm53m9.jpg' className='hover:animate-pulse w-full h-screen'/>
             </div>
             </Fade> 
-            </div>         
-        </div>  
-        <div className='md:hidden grid md:grid-cols-2'>
-        <div>
-              <Fade right>
-            <div class="h-screen flex flex-col relative  justify-center items-center">
-                <img src='https://res.cloudinary.com/dbwqjycvh/image/upload/v1680512780/boyenjoy_bm53m9.jpg' className='hover:animate-pulse w-full h-screen'/>
-            </div>
-            </Fade> 
-            </div>   
-            <div>  
-            <div className="flex flex-col relative  justify-center items-center shadow-xl my-4">
-            <Fade left>         
-              
-                 <h1 className="text-[#525252] text-center sm:text-5xl text-5xl font-SignikaNegative relative z-20">What We Do?</h1>
-                <p className="mx-4 text-[#525252] text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">At Sportin Leisure, we specialize in creating exceptional leisure and entertainment destinations for our clients. We understand the importance of understanding our clients' needs, which is why we work closely with them to understand their vision, project brief, and ideal budget. We also take into consideration the target clientele, project feasibility, and site analysis to cater to the demographic.
-                </p>
-                <p className="mx-4 text-[#525252] text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">
-We create and ideate by focusing on brand value, master planning, conceptualization, themed ambiances, schematic and detailed design. Our aim is to realize the creation by providing design supervision, project management, commissioning, pre-opening, and soft launch services.
-</p>
-<p className="text-[#525252] mx-4 text-justify mt-10 sm:text-lg text-lg font-SignikaNegative relative z-20">
-We provide post-launch support, including operations management, revenue analysis, marketing and promotion, and maximizing revenues. Our goal is to ensure that our clients' leisure facilities are not just operational but successful in the long term.</p>
-                </Fade>
-
-            </div> 
-            </div>
-                 
-        </div>            
+            </div>  
+                
+        </div>       
     </div>
   )
 }
